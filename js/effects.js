19bd3948579ca7f4b97bb6ce719138d1c67433fb
fernandopal <mail@fernandopal.es>
2020-11-05 14:29:17 +0100

    Updated files via upload

--- js/effects.js
+++ js/effects.js
@@ -3,7 +3,7 @@ $(document).on('click', '#goDown', function(){
 });
 
 if ($(".subtitle").length == 1) { 
-    var typed_strings = "Computer Technician, Web Developer"; 
+    var typed_strings = "Web Developer, Systems and Networks Technician"; 
     var typed = new Typed(".subtitle", { 
         strings: typed_strings.split(", "), 
         typeSpeed: 50, 
