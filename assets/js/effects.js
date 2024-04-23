69ddc366f1144b5ddc5ee9528d87f68cbc2aebbb
Fernando Palomo <mail@fpalomo.es>
2024-04-23 02:17:26 +0200

    Update website info

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,14 +1,3 @@
 $(document).on('click', '#goDown', function(){
     fullpage_api.moveSectionDown();
 });
-
-if ($(".subtitle").length == 1) { 
-    var typed_strings = "Salesforce Developer, Web Developer"; 
-    var typed = new Typed(".subtitle", { 
-        strings: typed_strings.split(", "), 
-        typeSpeed: 50, 
-        loop: true, 
-        backDelay: 900, 
-        backSpeed: 30, 
-    }); 
-}
